1c902d2ef1b4fc099ff7c91af816164b828a9eb4
Sergei Fedorov <s.fedorov.mhi@yandex.ru>
2023-06-25 18:02:57 +0300

    correct api views

--- guides/urls.py
+++ guides/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GuidesList, GuideElementDetail
+from .views import GuidesList, GuideElementList, GuideElementCheckOut
 
 urlpatterns = [
     path('refbooks/', GuidesList.as_view()),
-    path('refbooks/<int:pk>/elements', GuideElementDetail.as_view()),
+    path('refbooks/<int:pk>/elements', GuideElementList.as_view()),
+    path('refbooks/<int:pk>/check_element', GuideElementCheckOut.as_view()),
 ]
--- guides/views.py
+++ guides/views.py
@@ -1,13 +1,14 @@
+from django.db.models import Q
 from rest_framework import generics, mixins
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Guide, Element, Version
 from .serializers import GuideSerializer, ElementSerializer
 
 
-class GuidesList(mixins.ListModelMixin, generics.GenericAPIView):
-    serializer_class = GuideSerializer
-
+class GuidesList(APIView):
     def get_queryset(self):
         requested_date = self.request.query_params.get('date')
         if requested_date:
@@ -23,9 +24,7 @@ class GuidesList(mixins.ListModelMixin, generics.GenericAPIView):
         return Response(json_response)
 
 
-class GuideElementDetail(mixins.ListModelMixin, generics.GenericAPIView):
-    serializer_class = ElementSerializer
-
+class GuideElementList(APIView):
     def get_queryset(self):
         guide_id = self.kwargs.get('pk')
         version = self.request.query_params.get('version')
@@ -43,3 +42,25 @@ class GuideElementDetail(mixins.ListModelMixin, generics.GenericAPIView):
         serializer = ElementSerializer(queryset, many=True)
         json_response = {"elements": serializer.data}
         return Response(json_response)
+
+
+class GuideElementCheckOut(APIView):
+    def get_queryset(self):
+        guide_id = self.kwargs.get('pk')
+        version = self.request.query_params.get('version')
+        code = self.request.query_params.get('code')
+        value = self.request.query_params.get('value')
+        guide = get_object_or_404(Guide, pk=guide_id)
+        if version is None:
+            version_id = guide.current_version()
+        else:
+            version_id = Version.objects.filter(guide_id=guide_id, version=version).first()
+
+        queryset = Element.objects.filter(version_id=version_id, code=code, value=value)
+        return queryset
+
+    def get(self, request, pk):
+        queryset = self.get_queryset()
+        if queryset.exists():
+            return Response({'Element': 'Существует'})
+        return Response({'Element': 'Не существует'})
